d5fe3cda3dd05c676dde1f001a36a785c6429d8a
davidkaggulire1 <dkaggulire@gmail.com>
2023-01-21 22:46:16 +0300

    edit index file

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h2>Flask Vercel </h2>"
+# @app.route("/")
+# def home():
+#     return "<h2>Flask Vercel </h2>"
 
+"""run.py"""
+from api import app
+
+# app = create_app(os.environ.get('environment_variable')or 'testing')
+
+if __name__ == '__main__':
+    app.run(debug=True)
