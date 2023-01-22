e01b126fa23b021d703666e0d474c51312391340
davidkaggulire1 <dkaggulire@gmail.com>
2023-01-22 12:15:28 +0300

    remove unused functions

--- api/routes/admin_routes.py
+++ api/routes/admin_routes.py
@@ -1,12 +1,7 @@
 """admin_routes"""
 
 import os
-from flask_restful import Resource, reqparse
-from flask import jsonify, request, make_response
-# from flask_jwt_extended import jwt_required
-# from api.serializers.vehicle_serializer import response_serializer
-
-# from schemas.admin_schema import SignedoutSchema
+from flask_restful import Resource
 from decorators.decorators import api_key_required
 
 from dotenv import load_dotenv
@@ -22,24 +17,11 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 from api import api
-# from ..models import Vehicle
-
-
-BLANK = "'{}' cannot be blank"
-
-_parser = reqparse.RequestParser()
-_parser.add_argument('service', type=str,
-                     help=BLANK.format("service"))
-_parser.add_argument('fee', type=str,
-                     help=BLANK.format("fee"))
 
 
 class Materials(Resource):
     """signed out vehicles"""
 
-    # @jwt_required()
-    # @token_required
-    # @required_params(SignedoutSchema())
     @api_key_required
     def get(self):
         data = supabase.table("materials").select("*").execute().data
@@ -53,6 +35,7 @@ class Materials(Resource):
 class Home(Resource):
     def get(self):
         return 'Home Page Route'
+
 
 api.add_resource(Materials, "/api/v1/materials")
 api.add_resource(Home, "/")
